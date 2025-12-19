e0c5830f49b7ddaf6b29c9356e98a1d50750b381
ErikaContreras15 <84852247+ErikaContreras15@users.noreply.github.com>
2025-12-19 14:54:05 -0500

    Controlador inicial para movimientos caja
    Se crea el controlador base de movimientos de caja e incluye el método getAll para obtener registros existentes.

--- src/app/logistics/controllers/CashMovements.controller.ts
+++ src/app/logistics/controllers/CashMovements.controller.ts
@@ -2,5 +2,14 @@ import { Request, Response } from 'express';
 import CashMovements from '../../../data/models/logistics/CashMovements.model';
 
 export class CashMovementsController {
-    
-} 
+
+  static async getAll(req: Request, res: Response) {
+        try {
+            const movements = await CashMovements.findAll();
+            res.json(movements);
+        } catch (error) {
+            res.status(500).json({ errors: error });
+        }
+    }
+
+}
